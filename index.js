4b80b7471ee76c98088b1610eb93470923caa664
alfredo gimenez <alfredo@archibinario.com>
2021-04-09 17:57:55 +0200

    commit inicial

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3030;
+
+app.get('/', (req, res, next) => {
+    res.send('hola mundo');
+})
+
+app.listen(PORT, () => {
+    console.log(`app listening port ${PORT}`);
+});
